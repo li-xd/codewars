91f0008ecaacfa5c1b50069892c75be741500e5e
xiaodong li <a5834099147@live.cn>
2017-12-22 03:17:30 +0000

    Object-Oriented PHP #1 - Classes, Public Properties and Methods

---
+++ Code/ObjectOrientedClassesPublicPropertiesAndMethods.php
@@ -0,0 +1,9 @@
+<?php
+namespace Code;
+
+class ObjectOrientedClassesPublicPropertiesAndMethods {
+    
+}
+
+global $object_oriented_classes_public_properties_and_methods;
+$object_oriented_classes_public_properties_and_methods = new ObjectOrientedClassesPublicPropertiesAndMethods();
---
+++ Tests/ObjectOrientedClassesPublicPropertiesAndMethodsTest.php
@@ -0,0 +1,16 @@
+<?php
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+use Code;
+
+class ObjectOrientedClassesPublicPropertiesAndMethodsTest extends TestCase {
+  public function testThatPresidentClassExists() {
+    $this->assertTrue(class_exists('Code\ObjectOrientedClassesPublicPropertiesAndMethods'));
+  }
+  
+  public function testThatUSPresidentIsInstanceOfPresidentClass() {
+    global $object_oriented_classes_public_properties_and_methods;
+    $this->assertInstanceOf('Code\ObjectOrientedClassesPublicPropertiesAndMethods', $object_oriented_classes_public_properties_and_methods);
+  }
+}
